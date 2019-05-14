89ff88b2baff56e58aa75a67aaa700c94e210a46
Marcelo F. Cândido <marmacedoo9@gmail.com>
2019-05-14 16:21:51 -0300

    trying to complete the exercise

--- memory/logical-to-physical-addresses/src/logical-to-physical-addresses.c
+++ memory/logical-to-physical-addresses/src/logical-to-physical-addresses.c
@@ -5,19 +5,26 @@
 int main(int argc, char const *argv[]) {
 
   int page_size, offset, offset_bits;
-  long int page;
+  long unsigned int page;
+  int exit_program = 1;
 
   page_size = getpagesize();
   offset = sqrt(page_size);
   offset_bits = sqrt(offset);
-  page = ((int) &page_size) >> offset_bits;
+  page = ((int)&page_size) >> offset_bits;
 
-  printf("The actual page size is: %d bytes.\n", page_size);
+  printf("Hi, I'm the process %d.\n", getpid());
+  printf("The actual page size is: %d bytes. ", page_size);
   printf("That is 2^%d bytes.\n", offset);
-  printf("So, the offset is %d bits.\n", (int) sqrt(offset));
+  printf("So, the offset is %d bits.\n\n", (int)sqrt(offset));
 
-  printf("The variable 'page_size' has the logical address: .%p\n", &page_size);
+  printf("The variable 'page_size' has the logical address: %d.\n",
+         (unsigned int)&page_size);
   printf("So, the page address is %ld.\n", page);
+
+  while (exit_program) {
+    scanf("%d", &exit_program);
+  }
 
   return 0;
 }
